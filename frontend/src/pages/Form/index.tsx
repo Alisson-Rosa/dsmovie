4a8b282b966a9da6f6b39ea7e739893c5fca157c
Alisson Rosa <alissonrosa3030@gmail.com>
2022-01-12 20:58:43 -0300

    Security config

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -35,7 +35,7 @@ function Form() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <Link t o="/"> 
+                <Link to="/"> 
                 <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
                 </Link>
             </div >
