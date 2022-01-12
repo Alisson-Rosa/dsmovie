43403b9797e54a0b0675a381676f37b07a480cac
Alisson Rosa <alissonrosa3030@gmail.com>
2022-01-11 21:54:36 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -35,7 +35,7 @@ function Form() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <Link to="/"> 
+                <Link t o="/"> 
                 <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
                 </Link>
             </div >
