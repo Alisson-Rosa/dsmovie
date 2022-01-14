cde4126aa9e238fbb60c2409d9828bab26b75a52
Alisson Rosa <alissonrosa3030@gmail.com>
2022-01-13 23:24:22 -0300

    Erro

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,21 +1,21 @@
 import{ReactComponent as GithubIcon} from "assets/img/github.svg";
-import logo from './logo.svg';
+
 import './styles.css';
 function Navbar(){
-    return (
-    <header>
-    <nav className="container">
-      <div className="dsmovie-nav-content"> 
-        <h1>DSmovie</h1>
-        <a href="https://github.com/alisson-rosa">
-          <div className="dsmovie-contact-container">
-            <GithubIcon/>
-            <p className="dsmovie-contact-link">/devsuperior</p>
+  return (
+      <header>
+        <nav className="container">
+          <div className="dsmovie-nav-content"> 
+            <h1>DSmovie</h1>
+            <a href="https://github.com/alisson-rosa">
+              <div className="dsmovie-contact-container">
+                <GithubIcon />
+                <p className="dsmovie-contact-link">/devsuperior</p>
+              </div>
+            </a>
           </div>
-        </a>
-      </div>
-    </nav>
-  </header>
+        </nav>
+      </header>
   );
 }
 
