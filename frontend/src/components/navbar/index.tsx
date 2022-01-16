64e1ea0572c6ce54a30cedace6097ee6b1a03a92
Alisson Rosa <alissonrosa3030@gmail.com>
2022-01-16 17:40:57 -0300

    Pequenas alt

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -6,11 +6,11 @@ function Navbar(){
       <header>
         <nav className="container">
           <div className="dsmovie-nav-content"> 
-            <h1>ARmovie</h1>
+            <h1>ARfilmes</h1>
             <a href="https://github.com/alisson-rosa">
               <div className="dsmovie-contact-container">
                 <GithubIcon />
-                <p className="dsmovie-contact-link">/devsuperior</p>
+                <p className="dsmovie-contact-link">/Alisson-Rosa</p>
               </div>
             </a>
           </div>
